2da243cdab7cf82a74828c5bb78e259ca1f6d270
Nicolas Allende <nicolas1671@yahoo.com.ar>
2026-01-12 23:03:47 -0300

    Conexion a BD

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,13 +2,36 @@ import { Module } from '@nestjs/common';
 import { UsersModule } from './modules/users/users.module';
 import { ProductsModule } from './modules/products/products.module';
 import { OrdersModule } from './modules/orders/orders.module';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { ConfigModule, ConfigService } from '@nestjs/config';
 //import { DateAdderInterceptor } from './interceptors/date-adder.interceptor';
 //import { APP_INTERCEPTOR } from '@nestjs/core';
 //import { AuthGuard } from './guards/auth.guard';
 //import { APP_GUARD } from '@nestjs/core';
 
 @Module({
-  imports: [UsersModule, ProductsModule, OrdersModule],
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+      envFilePath: './.env.development',
+    }),
+    TypeOrmModule.forRootAsync({
+      inject: [ConfigService],
+      useFactory: (ConfigService: ConfigService) => ({
+        type: 'postgres',
+        database: ConfigService.get<string>('DB_NAME'),
+        host: ConfigService.get<string>('DB_HOST'),
+        port: ConfigService.get<number>('DB_PORT'),
+        username: ConfigService.get<string>('DB_USER'),
+        password: ConfigService.get<string>('DB_PASSWORD'),
+        synchronize: true,
+        logging: true,
+      }),
+    }),
+    UsersModule,
+    ProductsModule,
+    OrdersModule,
+  ],
   controllers: [],
   providers: [
     //{
